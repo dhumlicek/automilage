a65599dca9be5faa1d7fbd2efae607a6332ea716
Derek Humlicek <dhumlicek@gmail.com>
2018-04-04 09:48:46 -0500

    Fixed fields from brand to color. Working vehicle controllers

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -69,6 +69,6 @@ class VehiclesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def vehicle_params
-      params.require(:vehicle).permit(:year, :brand, :make, :model, :owned)
+      params.require(:vehicle).permit(:year, :make, :model, :color, :owned)
     end
 end
--- app/views/vehicles/_vehicle.json.jbuilder
+++ app/views/vehicles/_vehicle.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! vehicle, :id, :year, :brand, :make, :model, :owned, :created_at, :updated_at
+json.extract! vehicle, :id, :year, :make, :model, :color, :owned, :created_at, :updated_at
 json.url vehicle_url(vehicle, format: :json)
